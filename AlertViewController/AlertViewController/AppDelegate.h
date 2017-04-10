f8a838578ee009c524205a247e5ab7f371eed1b3
shenn13 <shenn13@163.com>
2017-04-10 17:18:13 +0800

    个人感觉比较好用的AlertViewController

---
+++ AlertViewController/AlertViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AlertViewController
+//
+//  Created by shen on 17/4/10.
+//  Copyright © 2017年 shen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AlertViewController/AlertViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AlertViewController
+//
+//  Created by shen on 17/4/10.
+//  Copyright © 2017年 shen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
